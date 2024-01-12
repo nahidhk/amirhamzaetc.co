52ed513cdab5232bd65a8f844fb80b3034cfdfee
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-01-12 16:02:42 +0600

    new update

---
+++ app.js
@@ -0,0 +1,8 @@
+function clos(){
+ document.getElementById("menu_box").classList="menu-box nav-btn animate__animated animate__hinge"
+}
+
+function opne(){
+   document.getElementById("menu_box").classList="menu-box  animate__animated .nav .menu-box .nav-btn:hover animate__bounceIn"
+
+}
